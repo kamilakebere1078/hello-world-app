f03e03e4380a8b32406ee105716671732c62fb79
None <kamilakebere1078@bastion-fuchicorp-com.us-central1-b.c.geometric-sled-282503.internal>
2021-06-09 00:09:37 +0000

    kamila owner is added

--- deployments/docker/app.py
+++ deployments/docker/app.py
@@ -6,7 +6,7 @@ def hello_world():
     return jsonify({
         'message': 'hello world',
         'environment': os.environ.get('ENVIRONMENT'),
-        'owner': 'kamilakebere1078',
+        'owner': 'kamila',
         'namespace': os.environ.get('NAMESPACE')
     })
 @app.route('/soon')
